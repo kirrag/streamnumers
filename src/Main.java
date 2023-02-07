4c6516d35463db616bc738e28936aba603c81291
Garrik Ten <ten@otux.ru>
2023-02-07 19:47:52 +0300

    source formated

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ import java.util.Collections;
 
 public class Main {
 	public static void main(String[] args) {
-		List<Integer> intSourceList = Arrays.asList( 1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4 );
+		List<Integer> intSourceList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
 
 		// Выделение положительных элементов из списка
 
@@ -16,7 +16,7 @@ public class Main {
 
 		while (iterator0.hasNext()) {
 			int item = (int) iterator0.next();
-			if(item > 0) {
+			if (item > 0) {
 				intPositiveList.add(item);
 			}
 		}
@@ -29,14 +29,13 @@ public class Main {
 
 		while (iterator1.hasNext()) {
 			int item = (int) iterator1.next();
-			if(item % 2 == 0) {
+			if (item % 2 == 0) {
 				intEvenList.add(item);
 			}
 		}
-		
+
 		// Сортировка списка
 		Collections.sort(intEvenList);
-
 
 		// Вывести список
 		System.out.println(intEvenList);
--- src/StreamMain.java
+++ src/StreamMain.java
@@ -5,13 +5,13 @@ import java.util.Comparator;
 
 public class StreamMain {
 	public static void main(String[] args) {
-		List<Integer> intList = Arrays.asList( 1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4 );
+		List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
 
 		List<Integer> result = intList.stream()
-			.filter( x -> x > 0 )
-			.filter( x -> x % 2 == 0 )
-			.sorted(Comparator.naturalOrder())
-			.collect(Collectors.toList());
+				.filter(x -> x > 0)
+				.filter(x -> x % 2 == 0)
+				.sorted(Comparator.naturalOrder())
+				.collect(Collectors.toList());
 
 		System.out.println(result);
 	}
