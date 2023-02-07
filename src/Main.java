8c9882bf57c4614107f26caca390df27174caf0b
Garrik Ten <ten@otux.ru>
2023-02-07 19:45:59 +0300

    streamnumers version 1

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Collections;
+
+public class Main {
+	public static void main(String[] args) {
+		List<Integer> intSourceList = Arrays.asList( 1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4 );
+
+		// Выделение положительных элементов из списка
+
+		List<Integer> intPositiveList = new ArrayList<>();
+
+		Iterator iterator0 = intSourceList.iterator();
+
+		while (iterator0.hasNext()) {
+			int item = (int) iterator0.next();
+			if(item > 0) {
+				intPositiveList.add(item);
+			}
+		}
+
+		// Выделение четных элементов из списка
+
+		List<Integer> intEvenList = new ArrayList<>();
+
+		Iterator iterator1 = intPositiveList.iterator();
+
+		while (iterator1.hasNext()) {
+			int item = (int) iterator1.next();
+			if(item % 2 == 0) {
+				intEvenList.add(item);
+			}
+		}
+		
+		// Сортировка списка
+		Collections.sort(intEvenList);
+
+
+		// Вывести список
+		System.out.println(intEvenList);
+	}
+}
---
+++ src/StreamMain.java
@@ -0,0 +1,18 @@
+import java.util.List;
+import java.util.Arrays;
+import java.util.stream.*;
+import java.util.Comparator;
+
+public class StreamMain {
+	public static void main(String[] args) {
+		List<Integer> intList = Arrays.asList( 1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4 );
+
+		List<Integer> result = intList.stream()
+			.filter( x -> x > 0 )
+			.filter( x -> x % 2 == 0 )
+			.sorted(Comparator.naturalOrder())
+			.collect(Collectors.toList());
+
+		System.out.println(result);
+	}
+}
